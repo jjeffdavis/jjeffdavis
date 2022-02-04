1f5c4c52cb3071e218cba625553693113ee8f1b9
jjd <jjd@Utrecht.local>
2022-02-03 19:02:44 -0800

    xampp reinstalled successfully.  sql nation built.

---
+++ index3.php
@@ -0,0 +1,10 @@
+<?php
+echo "<html>\n";
+echo "<head>\n";
+echo "<title>My Second PHP Example</title>\n";
+echo "</head>\n";
+echo "<body>\n";
+echo "<p>this is index3.php</p>\n";
+echo "</body>\n";
+echo "</html>\n";
+?>
