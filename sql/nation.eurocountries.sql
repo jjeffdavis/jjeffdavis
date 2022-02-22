26c3fe9bcf6dfb81d57463501a1f0b89d28d2f2a
jjd <jjd@Utrecht.local>
2022-02-22 01:33:07 -0800

    .modify css in not waving but drowning

--- sql/nation.eurocountries.sql
+++ sql/nation.eurocountries.sql
@@ -15,4 +15,30 @@ order by c.name;
 
 
 
-select * from continents;
+/*. nation.world.euro.misc.sql */
+
+select c.name, r.name 
+  from continents k, regions r, countries c
+  where
+  k.continent_id = r.continent_id and
+  r.region_id = c.region_id and
+  k.name = 'Europe'
+  order by c.name;
+
+
+  Select table1.ID ,table1.Name 
+from Table1 inner join Table2 on Table1 .ID =Table2 .ID  
+inner join Table3 on table2.ID=Table3 .ID 
+
+Select countries.name, regions.name, continents.name
+from regions inner join countries on regions.region_id = countries.region_id  
+inner join continents on continents.continent_id = regions.continent_id
+order by countries.name;
+
+
+/*. join world and nation. */
+select nation.countries.name, nation.countries.area, world.country.HeadOfState
+from nation.countries 
+inner join world.country 
+on nation.countries.country_code3   = world.country.Code
+order by nation.countries.name;
