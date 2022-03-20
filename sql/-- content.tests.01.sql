2ebe222b790a2fc577e91f4e3d22d79b211b026b
jjd <jjd@Utrecht.local>
2022-03-20 16:20:26 -0700

    git message is ready to work on production php exercises for class

---
+++ sql/-- content.tests.01.sql
@@ -0,0 +1,26 @@
+-- content.tests.01.sql
+
+
+  INSERT INTO `ctext` VALUES(4,"
+  
+
+  INSERT INTO \`ctext\` VALUES(3\,
+  \"
+  ---------------------------------------------
+>date format
+---------------------------------------------
+date \'+DATE: %m/%d/%y%nTIME:%H:%M:%S\' - Would list the time 
+and date in the below format:
+
+DATE: 02/08/01
+TIME:16:44:55
+---------------------------------------------
+\")\;
+
+INSERT INTO \`ckeys\` VALUES
+  (\"bash\"\, 3)\,
+  (\"date\"\, 3)\;
+
+
+
+");
